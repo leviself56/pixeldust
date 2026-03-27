871e1c5a65890fc8a01dc1a051c3ba8d049a27cd
leviself56 <160255898+leviself56@users.noreply.github.com>
2026-03-27 10:32:24 -0500

    Create config.php

---
+++ _libraries/config.php
@@ -0,0 +1,22 @@
+<?php
+
+return array (
+  'db' => 
+  array (
+    'host' => '127.0.0.1',
+    'port' => 3306,
+    'name' => 'pixeldust',
+    'user' => 'user',
+    'pass' => 'pass',
+    'charset' => 'utf8mb4',
+  ),
+  'app' => 
+  array (
+    'name' => 'Pixel Dust',
+    'session_name' => 'pixeldust_session',
+    'timezone' => 'America/Chicago',
+    'access_log_path' => '/var/www/site.com/site.com-ssl-access.log',
+  ),
+);
+
+?>
